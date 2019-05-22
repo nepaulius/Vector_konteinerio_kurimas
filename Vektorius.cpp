7ecaa8bbd260e3b805b6c141004e8b8ccee427de
GytisMil <gytis.miliauskas@mif.vu.lt>
2019-05-22 09:54:36 +0300

    Sukurti testavimai pagal reikalavima

--- Vektorius.cpp
+++ Vektorius.cpp
@@ -3,17 +3,45 @@
 #include <vector>
 #include <chrono>
 #include <algorithm>
+#include <chrono>
 #include "Vector.h"
 using std::cout;
 using std::endl;
 int main() {
-    unsigned int sz = 100000000;
     int k = 0;
-    Vector<int> v1 {9, 8, 7, 6, 5, 4, 3, 2, 1};
-    for (int i = 0; i < v1.size(); i++)
-        cout << v1[i] << endl;
-    std::sort(v1.begin(), v1.end());
-    for (int i = 0; i < v1.size(); i++)
-        cout << v1[i] << endl;
+    Vector<int> a {1, 2, 3};
+    Vector<int> b(std::move(a));
+    for (int i = 0; i < 3; i++)
+        cout << b[i] << endl;
+
+    Vector<int> v1;
+    std::chrono::high_resolution_clock::time_point start, ending;
+    start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < 100; i++)
+        v1.push_back(i);
+    ending = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<float> laikas = ending - start; //Gaunamas laiko rezultatas
+    cout << "Truko: " << laikas.count() << endl;
+    Vector<int> v2;
+    cout << v2.size() << " " << v2.capa() << endl;
+    v2.reserve(100);
+    cout << v2.size() << " " << v2.capa() << endl;
+    start = std::chrono::high_resolution_clock::now();
+        for (int i = 0; i < 100; i++)
+        v2.push_back(i);
+    ending = std::chrono::high_resolution_clock::now();
+    cout << "Truko: " << laikas.count() << endl;
+   // for (int i = 0; i < v1.size(); i++)
+      //  cout << v2[i] << endl;
+    cout << v2.size() << " " << v2.capa() << endl;
+    v2.shrink_to_fit();
+    cout << v2.size() << " " << v2.capa() << endl;
+
+    Vector<int> v3 {0, 1, 2, 3};
+   // cout << v3[5] << endl;
+    //cout << v3.at(5) << endl;
+
+   // Vector<int> v(9999999999);
+  //  Vector<int> v(99999999);
     return 0;
 }
